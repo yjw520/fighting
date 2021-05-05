de22166c06c27e54334e601b4fc9cd9b1c70588f
yjw <652321508@qq.com>
2021-05-05 23:44:48 +0800

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,12 @@
+// 连续子数组的最大和
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+    var sum=0
+    var nm=[]
+    for(let i=0;i<nums.length;i++){
+        sum=sum+nums[i]
+    } 
+};
